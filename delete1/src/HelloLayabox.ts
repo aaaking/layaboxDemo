da8d7b5f8e20703540f8b0b3a42ec57bfb93ef93
zhouzhihui <zhouzhihui@htgames.cn>
2018-03-27 18:00:01 +0800

    add HelloLayabox.ts

---
+++ delete1/src/HelloLayabox.ts
@@ -0,0 +1,6 @@
+console.info("Laya.init(960, 1080);");
+Laya.init(1280, 720);
+var txt = new Laya.Text();
+txt.text = "hello world"
+txt.color = "#FFFFFF"
+Laya.stage.addChild(txt)
--- delete1/src/main.ts
+++ delete1/src/main.ts
@@ -1,10 +1,12 @@
 // 程序入口
 class GameMain {
     constructor() {
-        Laya.init(600, 400);
+        Laya.init(1280, 720);
+        console.log("Laya.init(600, 400);");
     }
 }
 new GameMain();
+console.log("new GameMain();");
 
 // 1   .laya 文件夹下存放的是项目在开发运行中的一些配置信息。
 // 2   项目的输出目录（bin）
