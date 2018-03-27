9a75634a7a378c3ee0dadba34f1c8050a2c94bfe
zhouzhihui <zhouzhihui@htgames.cn>
2018-03-27 17:25:33 +0800

    git rm -r --cached .

--- delete1/bin/js/LayaSample.js
+++
@@ -1,9 +0,0 @@
-// 程序入口
-var GameMain = /** @class */ (function () {
-    function GameMain() {
-        Laya.init(1080, 920);
-    }
-    return GameMain;
-}());
-new GameMain();
-//# sourceMappingURL=LayaSample.js.map
