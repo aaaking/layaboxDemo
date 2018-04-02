43bbc1664943cee0840ccfc6f114bd784f771fe9
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-02 11:47:59 +0800

    add a new page for transforming

---
+++ delete1/src/pageTestComponent.ts
@@ -0,0 +1,22 @@
+module pageTestComponent {
+    export class ThisScene {
+        constructor() {
+            var scaleBtn = new ScaleButton("res/img/chip.png", "labelStr")
+            Laya.stage.addChild(scaleBtn)
+        }
+    }
+    class ScaleButton extends Laya.Button {
+        constructor(skin: string = null, label: string = "") {
+            super(skin, label)
+            this.on(Laya.Event.MOUSE_DOWN, this, this.small)
+            this.on(Laya.Event.MOUSE_UP, this, this.big)
+        }
+        private small() {
+            Laya.Tween.to(this, {scaleX: 0.8, scaleY: 0.8}, 300)
+        }
+        private big() {
+            Laya.Tween.to(this, {scaleX: 1, scaleY: 1}, 300)
+        }
+    }
+    new pageTestComponent.ThisScene()
+}
