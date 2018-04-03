11962cab17258652f3432a50c07b452a11c8657c
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-03 20:54:20 +0800

    test width and height

--- sgs_demo/bin/js/LayaSample.js
+++ sgs_demo/bin/js/LayaSample.js
@@ -4,12 +4,17 @@ var GameMain = /** @class */ (function () {
         //初始化引擎
         Laya.init(1280, 720, Laya.WebGL);
         Laya.stage.bgColor = "#ffffff";
-        Laya.stage.scaleMode = Laya.Stage.SCALE_FIXED_HEIGHT;
+        Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT;
         Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
         Dispatcher.init();
         Laya.loader.load([{ url: "config/hero.json", type: Laya.Loader.JSON }, { url: ResourceConfig.comp, type: Laya.Loader.ATLAS },
             { url: ResourceConfig.menu, type: Laya.Loader.ATLAS }, { url: ResourceConfig.cards, type: Laya.Loader.ATLAS }, { url: ResourceConfig.login, type: Laya.Loader.ATLAS },
             { url: ResourceConfig.cardsname, type: Laya.Loader.ATLAS }, { url: ResourceConfig.showcards, type: Laya.Loader.ATLAS }], Laya.Handler.create(this, this.onLoadCfgComplete));
+        //test code
+        Laya.stage.on(Laya.Event.RESIZE, this, function () {
+            console.log("browser width: " + Laya.Browser.width + " browser height" + Laya.Browser.height);
+            console.log("stage width: " + Laya.stage.width + " stage height" + Laya.stage.height);
+        });
     }
     GameMain.prototype.onLoadCfgComplete = function () {
         GameConfig.getCfg();
--- sgs_demo/src/LayaSample.ts
+++ sgs_demo/src/LayaSample.ts
@@ -1,24 +1,26 @@
 // 程序入口
-class GameMain{
-    constructor()
-    {
-       //初始化引擎
+class GameMain {
+    constructor() {
+        //初始化引擎
         Laya.init(1280, 720, Laya.WebGL);
-        Laya.stage.bgColor = "#ffffff";        
-        Laya.stage.scaleMode = Laya.Stage.SCALE_FIXED_HEIGHT;
+        Laya.stage.bgColor = "#ffffff";
+        Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT;
         Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
         Dispatcher.init();
 
-        Laya.loader.load([{url:"config/hero.json",type: Laya.Loader.JSON},{url:ResourceConfig.comp,type: Laya.Loader.ATLAS},
-        {url:ResourceConfig.menu,type: Laya.Loader.ATLAS},{url:ResourceConfig.cards,type: Laya.Loader.ATLAS},{url:ResourceConfig.login,type: Laya.Loader.ATLAS}
-        ,{url:ResourceConfig.cardsname,type: Laya.Loader.ATLAS},{url:ResourceConfig.showcards,type: Laya.Loader.ATLAS}],Laya.Handler.create(this,this.onLoadCfgComplete));
+        Laya.loader.load([{ url: "config/hero.json", type: Laya.Loader.JSON }, { url: ResourceConfig.comp, type: Laya.Loader.ATLAS },
+        { url: ResourceConfig.menu, type: Laya.Loader.ATLAS }, { url: ResourceConfig.cards, type: Laya.Loader.ATLAS }, { url: ResourceConfig.login, type: Laya.Loader.ATLAS }
+            , { url: ResourceConfig.cardsname, type: Laya.Loader.ATLAS }, { url: ResourceConfig.showcards, type: Laya.Loader.ATLAS }], Laya.Handler.create(this, this.onLoadCfgComplete));
+        //test code
+        // Laya.stage.on(Laya.Event.RESIZE, this, function () {
+        //     console.log("browser width: " + Laya.Browser.width + " browser height" + Laya.Browser.height)
+        //     console.log("stage width: " + Laya.stage.width + " stage height" + Laya.stage.height)
+        // })
     }
 
-    private onLoadCfgComplete():void{
+    private onLoadCfgComplete(): void {
         GameConfig.getCfg();
-
         SceneLogin.instance.show();
-      
     }
 
 }
