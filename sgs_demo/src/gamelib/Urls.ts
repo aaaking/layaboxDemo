47658fba3fa2e29cdf8a3823ff42dcce6a33573f
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-08 16:20:42 +0800

    remove some comment

--- sgs_demo/src/gamelib/Urls.ts
+++ sgs_demo/src/gamelib/Urls.ts
@@ -1,7 +1,7 @@
 class Urls {
     static personal_newAccount = "personal_newAccount"
     static personal_unlockAccount = "personal_unlockAccount"
-    static eth_sendTransaction = "eth_sendTransaction"//GameConfig.RPC_URL
+    static eth_sendTransaction = "eth_sendTransaction"
     static eth_call = "eth_call"
     static eth_getTransactionReceipt = "eth_getTransactionReceipt"
 }
