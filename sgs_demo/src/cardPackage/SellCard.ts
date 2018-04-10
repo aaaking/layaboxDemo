dc056353a33418dca960921321c4043d45c43de1
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-10 17:59:45 +0800

    sellcard pre refactor

--- sgs_demo/src/cardPackage/SellCard.ts
+++ sgs_demo/src/cardPackage/SellCard.ts
@@ -37,7 +37,6 @@ class SellCard extends ui.cardPackage.SellCardUI {
             case this._btnClose:
                 this.removeSelf();
                 break;
-
             case this._btnConfirm:
                 this.sell();
                 break;
@@ -50,63 +49,81 @@ class SellCard extends ui.cardPackage.SellCardUI {
     }
 
     private sell(): void {
+        let count = parseInt(this._input.text)
         let baseID = "0000000000000000000000000000000000000000000000000000000000000000"
         let baseCount = "0000000000000000000000000000000000000000000000000000000000000000"
-        if (this._input.text == "") {
+        this._boxNormal.mouseEnabled = false;
+        this._boxWaiting.visible = true;
+        this._boxWaiting.alpha = 0;
+        console.log("count: " + count)
+        console.log(this._data.id.toString(16))
+        console.log(count.toString(16).length)
+        console.log(baseCount.substring(0, 64 - this._data.id.toString(16).length) + this._data.id.toString(16))
+        let idstr = baseCount.substring(0, 64 - this._data.id.toString(16).length) + this._data.id.toString(16)
+        console.log(count.toString(16))
+        console.log(count.toString(16).length)
+        console.log(baseCount.substring(0, 64 - count.toString(16).length) + count.toString(16))
+        let countstr = baseCount.substring(0, 64 - count.toString(16).length) + count.toString(16)
+        console.log("countstr: " + countstr)
+        let param = String(idstr) + String(countstr)//param参数是0x+8位的方法名+价格的十六进制
+        console.log(param)
+        if (!count) {//if放到这里只是为了看log // todo
             Laya.Tween.clearTween(this._labTip);
             this._labTip.visible = true;
             this._labTip.alpha = 0;
-           
             Laya.Tween.to(this._labTip, { alpha: 1 }, 500, null, Laya.Handler.create(this, () => {
                 Laya.Tween.to(this._labTip, { alpha: 0 }, 500, null, null, 1000);
             }));
+            return
         }
-        else {
-            this._boxNormal.mouseEnabled = false;
-            this._boxWaiting.visible = true;
-            this._boxWaiting.alpha = 0;
-             let count = parseInt(this._input.text)
-             console.log("count: " + count)
-            console.log(this._data.id.toString(16))
-            console.log(count.toString(16).length)
-            console.log(baseCount.substring(0,64-this._data.id.toString(16).length)+this._data.id.toString(16))
-            let idstr = baseCount.substring(0,64-this._data.id.toString(16).length)+this._data.id.toString(16)
-            console.log(count.toString(16))
-            console.log(count.toString(16).length)
-            console.log(baseCount.substring(0,64-count.toString(16).length)+count.toString(16))
-            let countstr = baseCount.substring(0,64-count.toString(16).length)+count.toString(16)
-            console.log("countstr: " + countstr)
-            let param = String(idstr) + String(countstr)//param参数是0x+8位的方法名+价格的十六进制
-            console.log(param)
-            Laya.Tween.to(this._boxWaiting, { alpha: 1 }, 500, null);
-            // Laya.timer.once(5000, this, () => {
-            //     this._boxNormal.mouseEnabled = true;
-            //     this._boxWaiting.visible = false;
-            // })
-
-             Ajax.callNet(GameConfig.RPC_URL, { "jsonrpc": "2.0", "method": Urls.personal_unlockAccount, "params": [localStorage.getItem('uuid'), "", null
-                ], "id": 67 }, "POST", null, function (data) {
-                    console.info(data)
-                    Ajax.callNet(GameConfig.RPC_URL, { "jsonrpc": "2.0", "method": Urls.eth_sendTransaction, "params": [{"from":localStorage.getItem('uuid'), "to": GameConfig.RPC_ADDRESS, "data": "0x95bf05a4"+param }], "id": 67 }, "POST", null, function (data) {
+        Laya.Tween.to(this._boxWaiting, { alpha: 1 }, 500, null);
+        Ajax.callNet(GameConfig.RPC_URL, {
+            "jsonrpc": "2.0", "method": Urls.personal_unlockAccount, "params": [localStorage.getItem('uuid'), "", null], "id": 67
+        }, "POST", null, function (data) {
+            console.info(data)
+            Ajax.callNet(GameConfig.RPC_URL, { "jsonrpc": "2.0", "method": Urls.eth_sendTransaction, "params": [{ "from": localStorage.getItem('uuid'), "to": GameConfig.RPC_ADDRESS, "data": "0x95bf05a4" + param }], "id": 67 }, "POST", null, function (data) {
+                console.info(data)
+                var info = JSON.parse(data)
+                Laya.timer.loop(10000, this, function () {
+                    Ajax.callNet(GameConfig.RPC_URL, { "jsonrpc": "2.0", "method": Urls.eth_getTransactionReceipt, "params": [info.result], "id": 67 }, "POST", null, function (data) {
                         console.info(data)
-                        var info = JSON.parse(data)
-                        Laya.timer.loop(10000,this,function(){
-                            Ajax.callNet(GameConfig.RPC_URL, { "jsonrpc": "2.0", "method": Urls.eth_getTransactionReceipt, "params": [info.result], "id": 67 }, "POST", null, function (data) {
-                            console.info(data)
-                                let cardsinfo = JSON.parse(data)
-                                if(cardsinfo.result){
-                                    Laya.timer.clearAll(this)
-                                        this._boxNormal.mouseEnabled = true;
-                                        // this.showCard(id);
-                                        this._boxWaiting.visible = false;
-                                        this.removeSelf();
-                                         Dispatcher.dispatch("updateBag");
-                                }
-                            }.bind(this))
-                        })
+                        let cardsinfo = JSON.parse(data)
+                        if (cardsinfo.result) {
+                            Laya.timer.clearAll(this)
+                            this._boxNormal.mouseEnabled = true;
+                            this._boxWaiting.visible = false;
+                            this.removeSelf();
+                            Dispatcher.dispatch("updateBag");
+                        }
                     }.bind(this))
-                }.bind(this))
-            
+                })
+            }.bind(this))
+        }.bind(this))
+    }
+
+    private showLoading(success: boolean) {
+        if (success) {
+            this.requestNum = 0
+            this.requestIng = false
+        } else {
+            if (!this._boxWaiting.visible) {
+                this.mouseEnabled = false;
+                this._boxWaiting.visible = true;
+                this._boxWaiting.alpha = 0;
+                Laya.Tween.to(this._boxWaiting, { alpha: 1 }, 500, null);
+            }
         }
+    }
+
+    private onNetError(error?: any) {
+        this.requestNum = 0
+        this.requestIng = false
+        this.mouseEnabled = true;
+        this._boxWaiting.visible = false
+    }
+
+    requestNum = 0
+    requestIng: boolean = false
+    private getReceiptByLoop(info: any) {//轮询查询收据
     }
 }
