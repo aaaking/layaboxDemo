10e070556a4482681061fc375a6585ea6bf2194c
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-17 20:43:49 +0800

    android_offline

---
+++ sgs_demo/nativeOutput/android_offline/sgs_android_offline/android_studio/app/src/main/assets/scripts/config.js
@@ -0,0 +1,14 @@
+﻿var loadingView= window.loadingView;
+if(loadingView)
+{
+    loadingView.loadingAutoClose=true;
+	loadingView.showTextInfo=true;
+    loadingView.bgColor("#ffffff");
+    loadingView.setFontColor("#000000");
+    loadingView.setTips(["新世界的大门即将打开", "敌军还有30秒抵达战场", "妈妈说，心急吃不了热豆腐"]);
+}
+window.onLayaInitError=function(e)
+{
+	console.log("onLayaInitError error=" + e);
+	alert("加载游戏失败，可能由于您的网络不稳定，请退出重进");
+}
