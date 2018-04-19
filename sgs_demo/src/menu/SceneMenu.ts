5788ebb3c9c0a43589b8697e353b1b118549c847
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-19 11:45:03 +0800

    userbalance icon replace

--- sgs_demo/src/menu/SceneMenu.ts
+++ sgs_demo/src/menu/SceneMenu.ts
@@ -158,6 +158,7 @@ module menu {
             this._btnWarehouse = new menu.MenuCard("menu/menu_wareHouse.png")
             var whiteSpace = (Laya.stage.width - UITools.MAX_BG_WIDTH) >> 1
             this._balanceImg = new Laya.Image("menu/menu_icon_balance.png")
+            this._balanceImg.centerX = this._balanceImg.centerY = 0
             this._balanceBox = new Laya.Box()
             this._balanceBox.pos(Laya.stage.width - this._balanceImg.width - 16 - whiteSpace, 40)
             this._balanceBox.size(this._balanceImg.width, this._balanceImg.height)
